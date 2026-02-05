e45415ee24e069c9aeb0aafb23c194ecb1bcb22c
ksgoud4u <23311a05u4@cse.sreenidhi.edu.in>
2026-02-05 15:08:19 +0530

    Add files via upload

---
+++ lab3/script.js
@@ -0,0 +1,32 @@
+function detectDevice() {
+  const deviceTypeElement = document.getElementById("deviceType");
+  const featureList = document.getElementById("featureList");
+
+  if (window.innerWidth < 768) {
+    deviceTypeElement.textContent = "You are using a mobile device.";
+    featureList.innerHTML = `
+      <div class="bg-white p-4 border rounded shadow">Mobile Feature 1</div>
+      <div class="bg-white p-4 border rounded shadow">Mobile Feature 2</div>
+    `;
+  } 
+  else if (window.innerWidth < 1024) {
+    deviceTypeElement.textContent = "You are using a tablet.";
+    featureList.innerHTML = `
+      <div class="bg-white p-4 border rounded shadow">Tablet Feature 1</div>
+      <div class="bg-white p-4 border rounded shadow">Tablet Feature 2</div>
+      <div class="bg-white p-4 border rounded shadow">Tablet Feature 3</div>
+    `;
+  } 
+  else {
+    deviceTypeElement.textContent = "You are using a desktop.";
+    featureList.innerHTML = `
+      <div class="bg-white p-4 border rounded shadow">Desktop Feature 1</div>
+      <div class="bg-white p-4 border rounded shadow">Desktop Feature 2</div>
+      <div class="bg-white p-4 border rounded shadow">Desktop Feature 3</div>
+      <div class="bg-white p-4 border rounded shadow">Desktop Feature 4</div>
+    `;
+  }
+}
+
+window.onload = detectDevice;
+window.onresize = detectDevice;
